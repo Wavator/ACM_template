3b7f69da260d8197245ccb2a69222ded8d359744
Wavator <40030837+Wavator@users.noreply.github.com>
2018-06-14 16:23:12 +0800

    Update bit.cpp

--- data_struct/bit.cpp
+++ data_struct/bit.cpp
@@ -3,18 +3,30 @@ class fenwick {
 public:
     vector<T> fenw;
     int n;
-    fenwick(int _n) : n(_n) {
-        fenw.resize(static_cast<unsigned int>(_n));
-    }
-    void add(int x, const T &v) {
-        for (; x < n; x |= (x + 1))
-            fenw[x] += v;
+    fenwick(int _n=0) : n(_n) {
+        fenw.resize(n+1);
     }
 
-    T query(int x) {
-        T v{};
-        for (; x >= 0; x = (x & (x + 1)) - 1)
-            v += fenw[x];
-        return v;
+    void init(int n) {
+        this->n=n;
+        fenw.clear();
+        fenw.resize(n+1);
     }
+
+    inline void add(int pos,const T&val){
+        for(; pos <= n; pos += pos&-pos)
+            fenw[pos] += val;
+    }
+
+    inline T query(int pos){
+        T res{};
+        for(; pos; pos -= pos&-pos)
+            res += fenw[pos];
+        return res;
+    }
+
+    inline T query(int L, int R) {
+        return query(R) - query(L - 1);
+    }
+
 };
