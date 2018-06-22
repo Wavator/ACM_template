a634cb3afb286b028414605ce10e94ff927ebf19
Wavator <40030837+Wavator@users.noreply.github.com>
2018-06-22 08:55:52 +0800

    Update debug_cf.cpp

--- debug_cf.cpp
+++ debug_cf.cpp
@@ -1,8 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#pragma optimize("-O3")
-
 string to_string(string s) {
     return '"' + s + '"';
 }
@@ -11,7 +8,7 @@ string to_string(const char* s) {
     return to_string((string) s);
 }
 
-string to_string(const bool &b) {
+string to_string(bool b) {
     return (b ? "true" : "false");
 }
 
@@ -21,12 +18,13 @@ string to_string(pair<A, B> p) {
 }
 
 template <typename A>
-string to_string(const A &v) {
+string to_string(A v) {
     bool first = true;
     string res = "{";
     for (const auto &x : v) {
-        if (!first)
+        if (!first) {
             res += ", ";
+        }
         first = false;
         res += to_string(x);
     }
@@ -34,106 +32,85 @@ string to_string(const A &v) {
     return res;
 }
 
-void debug_out() { cerr << endl; }
+void my_debug() { cerr << endl; }
 
 template <typename Head, typename... Tail>
-void debug_out(Head H, Tail... T) {
+void my_debug(Head H, Tail... T) {
     cerr << " " << to_string(H);
-    debug_out(T...);
+    my_debug(T...);
 }
 
 #ifdef Wavator
-
-#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__);
-
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", my_debug(__VA_ARGS__);
 #else
-
 #define debug(...) 42;
-
 #define cerr if (false) cout
-
 #endif
 
-#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
-#define per(i, a, b) for (auto i = (b) - 1; i >= a; --i)
-
-#define pb push_back
-
 typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
 typedef double db;
-typedef vector <int> vi;
-typedef vector <ll> vl;
-typedef pair <int, int> pii;
-typedef pair <ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
 
-template <typename T>
-inline void _read(T &x) {
-    cin >> x;
+void R(){};
+
+template <typename Head, typename... Tail>
+void R(Head &H, Tail&...T) {
+    cin >> H;
+    R(T...);
+}
+template <class T>
+inline ostream& operator << (ostream &os, const vector<T> &o) {
+    auto it = o.begin();
+    os << *it;
+    for(++it;it!=o.end();++it)
+        os << ' ' << *it;
+    return os;
 }
 
-template <typename A, typename B>
-inline void _read(pair<A, B> &x) {
-    _read(x.first);
-    _read(x.second);
+template <class A, class B> ostream& operator << (ostream& out, const pair<A, B> &a) {
+    return out << a.first << ' ' << a.second;
 }
 
-template <typename T>
-inline void _read(vector<T> &x) {
-    for (auto &v: x)
-        _read(v);
-}
-
-
-void R() {}
-template <typename T, typename...U>
-void R(T &head, U&...tail) {
-    _read(head);
-    R(tail...);
-}
-
-#define endl '\n'
-
-template <typename T>
-inline void _write(const T &x) {
-    cout << x << ' ';
-}
-
-template <typename A, typename B>
-inline void _write(const pair<A, B>&x) {
-    _write(x.first);
-    _write(x.second);
-}
-
-template <typename T>
-inline void _write(const vector<T> &in) {
-    for (const auto &x: in)
-        _write(x);
-}
 
 void W() {cout << endl;}
 template <typename T, typename...U>
 void W(const T &head, const U&...tail) {
-    _write(head);
+    cout << head << ' ';
     W(tail...);
 }
 
-#define my_sort_unique(c) (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+template <typename T>
+inline bool ckmax(T &x, const T &y) {
+    return x < y? x = y, true: false;
+}
 
-#define my_unique(a) a.resize(distance(a.begin(), unique(a.begin(), a.end())))
-
-#define fi first
-
-#define se second
-
+template <typename T>
+inline bool ckmin(T &x, const T &y) {
+    return x > y? x = y, true: false;
+}
+mt19937 rnd((ull)time(0));
+void solve();
 
 int main() {
 #ifdef Wavator
     freopen("test.in", "r", stdin);
 #endif
     ios::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
-
-
-
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
     return 0;
+}
+
+const db eps = 1e-9, PI = acos(-1);
+const ll mod = 1e9 + 7, LL_INF = 0x3f3f3f3f3f3f3f3f;
+const int INT_INF = 0x3f3f3f3f;
+
+void solve() {
+
+    return;
 }
