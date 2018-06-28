ea040c8014fd388006efdd96973a2d57b86ce2d0
Wavator <40030837+Wavator@users.noreply.github.com>
2018-06-28 08:28:53 +0800

    Update 2B.cpp

--- io/2B.cpp
+++ io/2B.cpp
@@ -17,3 +17,34 @@ template <typename T> inline bool read(T &x) {
         x = -x;
     return true;
 }
+inline bool read(double &x) {
+    static char c;
+    for(;(c = gc()) != '-' && !isdigit(c);)
+        if (c == EOF) return false;
+    bool f = false;
+    if (c == '-')
+        x = 0, f = true;
+    else
+        x = c & 15;
+    for (;isdigit(c = gc());)
+        x = x * 10 + (c & 15);
+    if (c == '.') {
+        double base = 1.0;
+        while (isdigit(c = gc()))
+            x += (c & 15) * (base /= 10);
+    }
+    if (f)
+        x = -x;
+    return true;
+}
+inline bool read(char s[]) {
+    int ptr = 0;
+    static char ch;
+    for (ch = gc(); ch != ' ' && ch != '\n'; ch = gc()) {
+        if (ch == EOF)
+            return false;
+        s[ptr++] = ch;
+    }
+    s[ptr] = '\0';
+    return true;
+}
