c78c015171dd6fa0c56f56489b29e8dc6562d932
Wavator <40030837+Wavator@users.noreply.github.com>
2018-06-28 08:16:58 +0800

    Update 2B.cpp

--- io/2B.cpp
+++ io/2B.cpp
@@ -1,22 +1,19 @@
-char O[1<<20],*SS=O,*TT=O;
-#define gc (SS == TT && (TT = (SS = O) + fread(O, 1, 1 << 20, stdin), SS == TT)? -1: *SS++)
-
+inline char gc() {
+    static char O[1<<20], *SS = O, *TT = O;
+    return (SS == TT && (TT = (SS = O) + fread(O, 1, 1 << 20, stdin), SS == TT)? -1: *SS++);
+}
 template <typename T> inline bool read(T &x) {
     bool f = false;
     char c;
-    for (c = gc; !isdigit(c); c = gc) {
+    for (c = gc(); !isdigit(c); c = gc()) {
         if (c == EOF)
             return false;
-        if (c == '-')
+        else if (c == 45)
             f = true;
     }
-    for (x = 0; isdigit(c); c = gc) {
+    for (x = 0; isdigit(c); c = gc())
         x = x * 10 + c - 48;
-    }
-    if (f) {
+    if (f)
         x = -x;
-    }
     return true;
 }
-
-#undef gc
