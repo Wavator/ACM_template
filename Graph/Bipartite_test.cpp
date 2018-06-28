652be2b25baf964e1e108ea00e8d8dfba1257239
ZhaoYU <1012291737@qq.com>
2018-06-28 23:37:11 +0800

    edge*

--- Graph/Bipartite_test.cpp
+++ Graph/Bipartite_test.cpp
@@ -1,20 +1,21 @@
-const int N = 100005;
-vector<int> G[N];
-int c[N];
-bool dfs(int u, int co) {
-    c[u] = co;
-    for (auto &e: G[u]) {
-        if (!c[e] && !dfs(e, -co))
-            return false;
-        if (c[u] == c[e])
-            return false;
+    int color[N];
+    bool dfs(int u, int co) {
+        color[u] = co;
+        edge * t;
+        int v;
+        for (t = li[u]; t; t = t->next) {
+            v = t->y;
+            if (!color[v] && !dfs(v, -co))
+                return false;
+            if (color[u] == color[v])
+                return false;
+        }
+        return true;
     }
-    return true;
-}
-bool check(int n) {
-    memset(c, 0, sizeof(c));
-    for (int i = 1; i <= n; ++i)
-        if (c[i] == 0 && !dfs(i, 1))
-            return false;
-    return true;
-}
+    bool check(int n) {
+        memset(color, 0, sizeof(int)*(n+1));
+        for (int i = 1; i <= n; ++i)
+            if (color[i] == 0 && !dfs(i, 1))
+                return false;
+        return true;
+    }
