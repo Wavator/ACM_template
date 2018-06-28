056dbb7c6fecb73bd2e6af5bab70e78d34da2331
Wavator <zyzxjywy@163.com>
2018-06-28 23:56:30 +0800

    __test

--- Graph/tree_radius_count.cpp
+++ Graph/tree_radius_count.cpp
@@ -1,22 +1,19 @@
-    int count_radius() {
-        static int dep[N];
-        int mx = -1, pos = -1;
-        static function<void(int, int)> dfs = [&](int u, int fa) {
-            if(dep[u] > mx)
-                mx = dep[pos = u];
-            int v;
-            edge * t;
-            for(t = li[u]; t; t = t->next) {
-                v = t->y;
-                if (v != fa) {
-                    dep[v] = dep[u] + 1;
-                    dfs(v, u);
-                }
+int count_radius(const vector<int> e[]) {
+    static int dep[N];
+    int mx = -1, pos = -1;
+    static function<void(int, int)> dfs = [&](int u, int fa) {
+        if(dep[u] > mx)
+            mx = dep[pos = u];
+        for(int v: e[u]) {
+            if (v != fa) {
+                dep[v] = dep[u] + 1;
+                dfs(v, u);
             }
-        };
-        memset(dep, 0, sizeof(dep));
-        dfs(1, -1);
-        memset(dep, 0, sizeof(dep));
-        dfs(pos, -1);
-        return mx;
-    }
+        }
+    };
+    memset(dep, 0, sizeof(dep));
+    dfs(1, -1);
+    memset(dep, 0, sizeof(dep));
+    dfs(pos, -1);
+    return mx;
+}
