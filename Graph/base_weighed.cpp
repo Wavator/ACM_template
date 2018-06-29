6e4f02aaada822a7bf5f6057b3bd5cae77455b9d
Wavator <zyzxjywy@163.com>
2018-06-29 14:33:40 +0800

    x

--- Graph/base_weighed.cpp
+++ Graph/base_weighed.cpp
@@ -1,4 +1,5 @@
-struct Graph {
+class Graph {
+public:
     static const int N = ::N, M = N << 1;
     typedef long long T;
     struct edge {
@@ -11,13 +12,13 @@ struct Graph {
         memset(li, 0, sizeof(li));
         etop = 0;
     }
-    inline void add_edge(int u, int v, const T &w) {
+    inline void add_edge(int u, int v, const T &w = 0) {
         e[etop].y = v;
         e[etop].next = li[u];
         e[etop].w = w;
         li[u] = &e[etop++];
     }
-    inline void add_edges(int u, int v, const T &w) {
+    inline void add_edges(int u, int v, const T &w = 0) {
         add_edge(u, v, w);
         add_edge(v, u, w);
     }
