f227b181c60fc46f29d188a96f7811709705535d
Wavator <40030837+Wavator@users.noreply.github.com>
2018-07-15 13:41:17 +0800

    Update out.cpp

--- io/out.cpp
+++ io/out.cpp
@@ -1,23 +1,46 @@
-struct print_writer {
-    static const int OUT_LEN = 1<<20;
-    char obuf[OUT_LEN], *oh = obuf;
-    inline void print(char c) {
-        if (oh == obuf + OUT_LEN)
-            fwrite(obuf, 1, OUT_LEN, stdout), oh = obuf;
-        *oh++ = c;
-    }
+class print_writer {
+public:
     template<class T>
     inline void print(T x) {
-        static int buf[30], cnt;
+        static int a[30], cnt;
         if (x == 0) {
-            print('0');
+            pc(48);
         } else {
-            if (x < 0) print('-'), x = -x;
-            for (cnt = 0; x; x /= 10) buf[++cnt] = x % 10 + 48;
-            while (cnt) print((char)buf[cnt--]);
+            if (x < 0) pc(45), x = -x;
+            for (cnt = 0; x; x /= 10) a[++cnt] = x % 10 + 48;
+            for(;cnt;)
+                pc((char)a[cnt--]);
         }
     }
+    inline void print(const char * s) {
+        static int i;
+        for (i = 0; s[i]; ++i) pc(s[i]);
+    }
+    inline void print(char c) {
+        pc(c);
+    }
+    inline void print(const string &s) {
+        for (const auto&c : s) pc(c);
+    }
+    template <typename T> inline void println(const T&x) {
+        print(x), pc(10);
+    }
+    template <typename T> inline void prints(const T &x) {
+        print(x), pc(' ');
+    }
+    inline void flush() {
+        fwrite(buf, 1, OUT_LEN, stdout), oh = buf;
+        fflush(stdout);
+    }
+private:
+    static const int OUT_LEN = 1<<20;
+    static char buf[OUT_LEN], *oh = buf;
+    inline void pc(char c) {
+        if (oh == buf + OUT_LEN)
+            fwrite(buf, 1, OUT_LEN, stdout), oh = buf;
+        *oh++ = c;
+    }
     ~print_writer() {
-        fwrite(obuf, 1, oh - obuf, stdout);
+        fwrite(buf, 1, oh - buf, stdout);
     }
 }out;
