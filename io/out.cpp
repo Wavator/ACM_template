7b12450bec44851726032202abaf771572ad2151
Wavator <40030837+Wavator@users.noreply.github.com>
2018-07-20 22:07:57 +0800

    Update out.cpp

--- io/out.cpp
+++ io/out.cpp
@@ -1,47 +1,24 @@
 class print_writer {
 public:
     template<class T>
-    inline void print(T x) {
+    inline void p(T x) {
         static int a[30], cnt;
-        if (x == 0) {
-            pc(48);
-        } else {
-            if (x < 0) pc(45), x = -x;
-            for (cnt = 0; x; x /= 10) a[++cnt] = x % 10 + 48;
-            for(;cnt;)
-                pc((char)a[cnt--]);
+        if (x==0)pc(48);
+        else {
+            if(x<0)pc(45),x=-x;
+            for(cnt=0;x;x/=10)a[++cnt]=x%10+48;
+            for(;cnt;) pc(a[cnt--]);
         }
     }
-    inline void print(const char * s) {
-        static int i;
-        for (i = 0; s[i]; ++i) pc(s[i]);
-    }
-    inline void print(char c) {
-        pc(c);
-    }
-    inline void print(const string &s) {
-        for (const auto&c : s) pc(c);
-    }
-    template <typename T> inline void println(const T&x) {
-        print(x), pc(10);
-    }
-    template <typename T> inline void prints(const T &x) {
-        print(x), pc(' ');
-    }
-    inline void flush() {
-        fwrite(obuf, 1, OUT_LEN, stdout), oh = obuf;
-        fflush(stdout);
-    }
-    ~print_writer() {
-        fwrite(obuf, 1, oh - obuf, stdout);
-    }
-private:
-    static const int OUT_LEN = 1<<20;
-    char obuf[OUT_LEN], *oh = obuf;
-    inline void pc(char c) {
-        if (oh == obuf + OUT_LEN)
-            fwrite(obuf, 1, OUT_LEN, stdout), oh = obuf;
-        *oh++ = c;
-    }
-
-}out;
+    template <typename T>inline void p(T*b, const T*e){ if(e-b>0)for (p(*b++);b!=e;b++)p(' '),p(*b);}
+    inline void p(const char * s){static int i;for (i=0;s[i];++i)pc(s[i]);}
+    inline void p(char c){pc(c);}
+    inline void p(const string &s){for (const auto&c:s)pc(c);}
+    template <typename T> inline void pl(const T&x){p(x),pc(10);}
+    template <typename T> inline void ps(const T &x){p(x), pc(' ');}
+    inline void flush(){fwrite(_, 1, O, stdout), oh = _;fflush(stdout);}
+    inline void pc(char c){if (oh == _ + O)fwrite(_, 1, O, stdout), oh = _;*oh++ = c;}
+    ~print_writer(){fwrite(_, 1, oh - _, stdout);}
+    static const int O = 1<<20;
+    char _[O], *oh = _;
+}o;
