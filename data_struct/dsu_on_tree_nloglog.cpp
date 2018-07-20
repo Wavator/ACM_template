403207cf2b8ca87f30b5bf2e3492d68b612f8d76
Wavator <40030837+Wavator@users.noreply.github.com>
2018-07-21 00:59:32 +0800

    Update dsu_on_tree_nloglog.cpp

--- data_struct/dsu_on_tree_nloglog.cpp
+++ data_struct/dsu_on_tree_nloglog.cpp
@@ -82,3 +82,62 @@ void dfs(int u, int fa) {
 
     //cerr << u << ' ' << mp.query() << ' ' << dep[u] << endl;
 }
+
+///////////////////////////////
+struct my_map {
+    map<int, int> mp;
+    set<int> s;
+    ll sum;
+    int mx;
+    void insert(int color) {
+        mp[color]++;
+        int x = mp[color], y = mp[mx];
+        if (x > y) {
+            s.clear();
+            s.insert(color);
+            sum = color;
+            mx = color;
+        } else if (x == y && !s.count(color)) {
+            sum += color;
+            s.insert(color);
+        }
+    }
+    ll query() {
+        return sum;
+    }
+    bool operator < (const my_map &o) const {
+        return mp.size() < o.mp.size();
+    }
+    void operator += (const my_map &o) {
+        int pre_mx = mp[mx];
+        for (const auto &p: o.mp) {
+            mp[p.first] += p.second;
+            if (pre_mx < mp[p.first]) {
+                mx = p.first;
+                pre_mx = mp[p.first];
+                s.clear();
+                sum = p.first;
+                s.insert(p.first);
+            } else if (pre_mx == mp[p.first] && !s.count(p.first)) {
+                sum += p.first;
+                s.insert(p.first);
+            }
+        }
+    }
+};
+my_map mps[N];
+vector<int> e[N];
+int c[N];
+ll ans[N];
+void dfs(int u, int fa) {
+    mps[u].insert(c[u]);
+    for (const auto &v: e[u]) {
+        if (v != fa) {
+            dfs(v, u);
+            if (mps[u] < mps[v])
+                swap(mps[u], mps[v]);
+            mps[u] += mps[v];
+        }
+    }
+    ans[u] = mps[u].query();
+}
