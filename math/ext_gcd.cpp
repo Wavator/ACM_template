76e8b5480a0b92a859bfd2b13a1159cfda7480cd
Wavator <40030837+Wavator@users.noreply.github.com>
2018-07-24 12:46:25 +0800

    Update ext_gcd.cpp

--- math/ext_gcd.cpp
+++ math/ext_gcd.cpp
@@ -1,13 +1,9 @@
-ll ext_gcd(ll a, ll b, ll &x, ll &y)
-{
-    ll d = a;
-    if(b) {
-        d = ext_gcd(b, a % b, y, x);
-        y -= (a / b) * x;
+ll ext_gcd(ll a, ll b, ll &d, ll &x, ll &y) {
+    if (b) {
+        ext_gcd(b, a % b, d, y, x);
+        y -= x * (a / b);
+    } else {
+        d = a, x = 1, y = 0;
     }
-    else {
-        x = 1;
-        y = 0;
-    }
-    return d;
 }
+
