3d895695f850153418d241d0e101ebf68dd11a5f
Wavator <40030837+Wavator@users.noreply.github.com>
2018-07-29 00:50:07 +0800

    Update normal.cpp

--- normal.cpp
+++ normal.cpp
@@ -9,24 +9,28 @@ using namespace std;
 #define fi first
 #define se second
 #ifdef Wavator
-#define debug(fmt, ...) fprintf(stderr, "%s: %s = " fmt "\n", __func__, #__VA_ARGS__, __VA_ARGS__), fflush(stderr)
+    #define debug(fmt, ...) fprintf(stderr, "func[%s] line %d: %s = " fmt "\n", \
+                __func__, __LINE__, #__VA_ARGS__, __VA_ARGS__), fflush(stderr)
+    #define err(x) cerr << "func[" << __func__ << "] line " << __LINE__ << ": " \
+                    << #x << " = " << x << endl << flush
 #else
-#define debug(...) 99
+    #define debug(...) 99
+    #define err(x) 99
 #endif
 typedef long long ll;
 typedef double db;
-typedef long double ld;
 typedef vector<int> vi;
 typedef pair<int, int> pii;
-const ll mod = (ll)1e9+7;
-ll pow_mod(ll a,ll b=mod-2){
-    assert(b>=0);ll res=1;for(a%=mod;b;b>>=1,a=a*a%mod)if(b&1)res=res*a%mod;
+const ll mod = (ll)1e9 + 7;
+ll pow_mod(ll a, ll b) { 
+    assert(b >= 0);ll res=1;
+    for (a %= mod; b; b >>= 1, a = a * a % mod) 
+        if (b & 1) res = res * a % mod; 
     return res;
 }
-
 int main() {
 #ifdef Wavator
-    freopen("test.in","r",stdin);
+    freopen("test.in", "r", stdin);
 #endif
 
     return 0;
