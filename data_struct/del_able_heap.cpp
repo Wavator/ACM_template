065ca812c5ff1caeac007758e9f44318f4a064a3
Wavator <40030837+Wavator@users.noreply.github.com>
2018-07-29 19:20:07 +0800

    Create del_able_heap.cpp

---
+++ data_struct/del_able_heap.cpp
@@ -0,0 +1,8 @@
+struct del_able_heap {
+    priority_queue<int> a, b;
+    inline void push(int x) { a.push(x); }
+    inline void erase(int x) { b.push(x); }
+    inline void maintain() { while(!b.empty()&&a.top()==b.top())a.pop(),b.pop(); }
+    inline void pop() {maintain();a.pop();}
+    inline int size() {return a.size()-b.size();}
+};
