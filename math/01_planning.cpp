9d07f17f5242f0414c6b7e63292629b099cc9baa
Wavator <40030837+Wavator@users.noreply.github.com>
2018-08-03 15:09:00 +0800

    Create 01_planning.cpp

---
+++ math/01_planning.cpp
@@ -0,0 +1,21 @@
+int n, k;
+
+int s[N], c[N];
+db f[N];
+int main() {
+    scanf("%d%d", &n, &k);
+    rep(i,0,n)scanf("%d",s+i);
+    rep(i,0,n)scanf("%d",c+i);
+    db l=0,r=2000;
+    rep(i,0,n)c[i]*=s[i];
+    while(fabs(r-l)>eps){
+        db mid=(l+r)/2.;
+        rep(i,0,n)f[i]=1.*c[i]-mid*s[i];
+        sort(f,f+n,greater<db>());
+        db s=0;
+        rep(i,0,k)s+=f[i]; // remain k elements to make c[i]/s[i] maximize.
+        if(s>0)l=mid;
+        else r=mid;
+    }
+    return !printf("%.10f\n",l);
+}
