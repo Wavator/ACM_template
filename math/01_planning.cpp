a3e8ddeaab08491375cd63ab13dd5e3c3573ef6d
Wavator <40030837+Wavator@users.noreply.github.com>
2018-08-03 15:23:05 +0800

    Update 01_planning.cpp

--- math/01_planning.cpp
+++ math/01_planning.cpp
@@ -1,20 +1,18 @@
-int n, k;
-
-int s[N], c[N];
-db f[N];
-int main() {
-    scanf("%d%d", &n, &k);
-    rep(i,0,n)scanf("%d",s+i);
-    rep(i,0,n)scanf("%d",c+i);
-    db l=0,r=*max_element(c,c+n);
-    while(fabs(r-l)>eps){
-        db mid=(l+r)/2.;
-        rep(i,0,n)f[i]=1.*c[i]-mid*s[i];
-        nth_element(f,f+k,f+n,greater<db>());
-        db s=0;
-        rep(i,0,k)s+=f[i];
-        if(s>-eps)l=mid;
-        else r=mid;
+template <size_t N, typename T, typename Z = double>
+struct zero_one_plan
+{
+    Z f[N];
+    Z solve(T *c, T *s, int n, int k) { // max-> sigma(c[i])/sigma(s[i])
+        Z l=0,r=*max_element(c,c+n);
+        while(fabs(r-l)>eps){
+            Z mid=(l+r)/2.;
+            rep(i,0,n)f[i]=1.*c[i]-mid*s[i];
+            nth_element(f,f+k,f+n,greater<Z>());
+            Z sm=0;
+            rep(i,0,k)sm+=f[i];
+            if(sm>-eps)l=mid;
+            else r=mid;
+        }
+        return l;
     }
-    return !printf("%.10f\n",l);
-}
+};
