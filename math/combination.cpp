34afce4dad66e820c408eb62e4ce6ba7714dee70
Wavator <40030837+Wavator@users.noreply.github.com>
2018-08-14 14:51:54 +0800

    Update combination.cpp

--- math/combination.cpp
+++ math/combination.cpp
@@ -1,25 +1,24 @@
-namespace comb
-{
-    const int N = ::N;
-    ll fact[N], finv[N];
-    void init()
-    {
+namespace comb {
+    const int N = 100005;
+    int fact[N], finv[N];
+    void init() {
         fact[0]=1;
-        rep(i,1,N)fact[i]=fact[i-1]*i%mod;
+        rep(i,1,N)fact[i]=mul(fact[i-1],i);
         finv[N-1]=pow_mod(fact[N-1],mod-2);
         per(i,0,N-1){
-            finv[i]=finv[i+1]*(i+1)%mod;
-            //assert(fact[i]*finv[i]%mod==1);
+            finv[i]=mul(finv[i+1],i+1);
+#ifdef Wavator
+            assert(fact[i]*finv[i]%mod==1);
+#endif
         }
     }
-    inline ll C(int n, int m)
-    {
-        return (fact[n] * finv[m] % mod) * finv[n - m] % mod;
+    inline int C(int n, int m) {
+        if (n < m) return 0;
+        return mul(fact[n], mul(finv[m], finv[n - m]));
     }
-    inline ll lucas(ll n, ll m) {
+    inline int lucas(int n, int m) {
         if(n < m) return 0;
-        if(!m || n == m)
-            return 1;
-        return C(n % mod,m % mod) * lucas(n / mod,m / mod) % mod;
+        if(!m || n == m) return 1;
+        return mul(lucas(n / mod,m / mod),C(n % mod, m % mod));
     }
 }
