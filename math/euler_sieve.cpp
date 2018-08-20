a51151030566a55b9f52661e4c2c2327628c2464
Wavator <40030837+Wavator@users.noreply.github.com>
2018-08-20 21:04:49 +0800

    Update euler_sieve.cpp

--- math/euler_sieve.cpp
+++ math/euler_sieve.cpp
@@ -1,60 +1,65 @@
-namespace euler_sieve
-{ // thanks @RoundGod
-    const int N = 1000005;
-    int prime[N];
-    int phi[N];
-    int miu[N];
-    bool is_prime[N];
-    int sieve(int n) {
-        int p=0;
-        fill(is_prime+2,is_prime+n+1,true);
-        for(int i=2;i<=n;i++) {
-            if(is_prime[i])
-                prime[p++]=i;
-            for(int j=0;j<p;j++) {
-                if(prime[j]*i>n) break;
-                is_prime[prime[j]*i]=false;
-                if(i%prime[j]==0) break;
-            }
-        }
-        return p;
-    }
-    void genphi(int n) {
-        int p=0;
-        memset(phi,0,sizeof(phi));
-        phi[1]=1;
-        for(int i=2;i<=n;i++) {
-            if(is_prime[i]) {
-                p++;
-                phi[i]=i-1;
-            }
-            for(int j=0;j<p;j++) {
-                if(prime[j]*i>n)
-                    break;
-                phi[i*prime[j]]=phi[i]*(i%prime[j]?prime[j]-1:prime[j]);
-                if(i%prime[j]==0)
-                    break;
-            }
+
+// thanks @RoundGod
+const int N = 1000005;
+int prime[N],phi[N],miu[N];
+bool is_prime[N];
+
+int sieve(int n) {
+    int p = 0;
+    fill(is_prime + 2, is_prime + n + 1, true);
+    for(int i = 2; i <= n; i++) {
+        if(is_prime[i])
+            prime[p++] = i;
+        for(int j = 0; j < p; j++) {
+            if(prime[j] * i > n)
+                break;
+            is_prime[prime[j] * i] = false;
+            if(i % prime[j] == 0)
+                break;
         }
     }
-    void genmiu(int n) {
-        int p=0;
-        memset(miu,0,sizeof(miu));
-        miu[1]=1;
-        for(int i=2;i<=n;i++)
+    return p;
+}
+
+void gen_phi(int n) {
+    int p = 0;
+    fill(phi, phi + n + 1, 0);
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        if (is_prime[i]) {
+            p++;
+            phi[i] = i - 1;
+        }
+        for (int j = 0; j < p; j++)
         {
-            if(is_prime[i]) {
-                p++;
-                miu[i]=-1;
-            }
-            for(int j=0;j<p;j++)
-            {
-                if(prime[j]*i>n)
-                    break;
-                miu[i*prime[j]]=i%prime[j]?-miu[i]:0;
-                if(i%prime[j]==0)
-                    break;
-            }
+            if (prime[j] * i > n)
+                break;
+            phi[i * prime[j]] = phi[i] * (i % prime[j]? prime[j] - 1: prime[j]);
+            if (i % prime[j] == 0)
+                break;
         }
     }
 }
+void gen_miu(int n)
+{
+    int p=0;
+    fill(miu, miu + n + 1, 0);
+    miu[1]=1;
+    for (int i = 2; i <= n; i++)
+    {
+        if (is_prime[i]) {
+            p++;
+            miu[i] = -1;
+        }
+        for (int j = 0; j < p; j++)
+        {
+            if (prime[j] * i > n)
+                break;
+            miu[i * prime[j]] = i % prime[j]? -miu[i]: 0;
+            if(i % prime[j] == 0)
+                break;
+        }
+    }
+}
+
