313df6ef510e7751292c229e51fea70125b436b4
Wavator <40030837+Wavator@users.noreply.github.com>
2018-08-24 02:17:10 +0800

    Update vtree.cpp

--- data_struct/vtree.cpp
+++ data_struct/vtree.cpp
@@ -1,87 +1,81 @@
-const int N = 250010, LOG = 20;
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N = 100005;
+vector<int> g[N],g2[N];
+int depth = 0,bn = 0,b[N << 1];
+int f[N << 1],dfn[N],dis[N],fs[N];
+ll ans[N];
 
-struct edge {
-    int v;
-    ll w;
-    edge(int v = 0, int w = 0): v(v), w(w){}
-};
-
-vector<edge> e[N];
-vi g[N];
-
-int pos[N], fa[N][LOG], dep[N];
-int clk;
-ll min_dis[N];
-void pre_dfs(int u) {
-    pos[u] = ++clk;
-    for(int i = 1; i < LOG; i++)
-        fa[u][i]=fa[fa[u][i-1]][i-1];
-    edge v;
-    for (int i = 0; i < SZ(e[u]); ++i) {
-        v = e[u][i];
-        if (v.v != fa[u][0]) {
-            min_dis[v.v] = min(min_dis[u], (ll) v.w);
-            dep[v.v] = dep[u] + 1;
-            fa[v.v][0] = u;
-            pre_dfs(v.v);
-        }
+void dfs(int u,int fa) {
+    int tmp = ++depth;
+    b[++bn] = tmp;
+    f[tmp] = u;
+    dfn[u] = bn;
+    for (auto v:g[u]) {
+        if (v == fa) continue;
+        dis[v] = dis[u] + 1;
+        dfs(v,u);
+        b[++bn] = tmp;
     }
 }
 
-void init_vtree() {
-    min_dis[1] = 1ll<<60;
-    pre_dfs(1);
+int st[N << 1][20];
+int lg[N << 1];
+void st_init() {
+    for (int i = 2; i < N * 2; ++i) lg[i] = lg[i >> 1] + 1;
+    for (int i = bn; i >= 1; --i) {
+        st[i][0] = b[i];
+        for (int j = 1; i + (1 << j) - 1 <= bn; ++j)
+            st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+    }
+}
+inline int rmq(int l, int r) {
+    int k = lg[r - l + 1];
+    return min(st[l][k], st[r - (1 << k) + 1][k]);
 }
 
-inline int LCA(int a,int b){
-    if (dep[a] > dep[b])
-        swap(a,b);
-    for (int i = dep[b] - dep[a], j = 0; i > 0;i >>= 1, j++)
-        if (i & 1)
-            b = fa[b][j];
-    if (a == b)
-        return a;
-    int k;
-    for (k = 0; (1 << k) <= dep[a]; k++);
-    for (; ~k; k--)
-        if ((1 << k) <= dep[a] && fa[a][k] != fa[b][k])
-            a = fa[a][k], b = fa[b][k];
-    return fa[a][0];
+inline int lca(int a,int b) {
+    if(a == b) return a;
+    if (dfn[a] > dfn[b]) swap(a,b);
+    int k = rmq(dfn[a],dfn[b]);
+    return f[k];
 }
 
-ll dp(int u) {
-    if (!SZ(g[u])) return min_dis[u];
-    ll res = 0;
-    for (int i = 0; i < SZ(g[u]); ++i) res += dp(g[u][i]);
-    g[u].clear();
-    return min(res, min_dis[u]);
+bool cmp(const int &i, const int &j) {
+    return dfn[i] < dfn[j];
 }
-
-inline void insert_vtree(int u, int v) {
-    if(u^v)g[u].push_back(v);
-}
-void build_vtree(int * h, int K, int root=1) {
-    sort(h+1,h+K+1,[&](const int &x, const int &y) {
-        return pos[x] < pos[y];
-    });
-    int tot=0;
-    h[++tot]=h[1];
-    for(int i=2;i<=K;i++)
-        if(LCA(h[tot],h[i])!=h[tot])h[++tot]=h[i];
-    static int st[N];
-    int top = 0;
-    st[++top]=root;
-    for(int i=1;i<=tot;i++) {
-        int now=h[i],f=LCA(now,st[top]);
-        for(;;) {
-            if(dep[f]>=dep[st[top-1]]) {
-                insert_vtree(f,st[top--]);
-                if(st[top]!=f)st[++top]=f;
-                break;
+//vec 为需要构建虚树的点集,构造出的虚树树根为0
+//原树不包含点0
+void build_tree(vector<int> &vec) {
+    int sz = 0,k = (int)vec.size();
+    sort(vec.begin(),vec.end(),cmp);
+    fs[sz] = 0;
+    g2[0].clear();
+    for (int i = 0; i < k; ++i) {
+        int u = vec[i], ll = lca(u, fs[sz]);
+        g2[u].clear();
+        if (ll == fs[sz]) fs[++sz] = u;
+        else {
+            while (sz >= 1 && dis[fs[sz - 1]] >= dis[ll]) {
+                g2[fs[sz - 1]].push_back(fs[sz]);
+                sz--;
             }
-            insert_vtree(st[top-1],st[top]);top--;
+            if (fs[sz] != ll) {
+                g2[ll].clear();
+                g2[ll].push_back(fs[sz--]);
+                fs[++sz] = ll;
+            }
+            fs[++sz] = u;
         }
-        if(st[top]!=now)st[++top]=now;
     }
-    while(--top)insert_vtree(st[top],st[top+1]);
+    for (int i = 0; i < sz; ++i) g2[fs[i]].push_back(fs[i + 1]);
+}
+
+int main() {
+    dis[1] = 1;
+    dfs(1,0);
+    st_init();
+    build_tree(vec);
+    return 0;
 }
