27268977d82a4efdeb10f6172e62bac8d39ef116
Wavator <40030837+Wavator@users.noreply.github.com>
2018-08-26 15:11:26 +0800

    Update normal.cpp

--- normal.cpp
+++ normal.cpp
@@ -1,5 +1,10 @@
 #pragma optimize("-O3")
 #include <bits/stdc++.h>
+#ifndef Wavator
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/hash_policy.hpp>
+#define unordered_map __gnu_pbds::cc_hash_table
+#endif
 using namespace std;
 #ifdef Wavator
 #include "local_debug.h"
@@ -13,7 +18,8 @@ using namespace std;
 #define SZ(x) ((int)(x).size())
 #define fi first
 #define se second
-typedef long long ll;
+typedef unsigned long long ll;
 typedef double db;
 typedef vector<int> vi;
+typedef vector<vi> vvi;
 typedef pair<int, int> pii;
