ebe891bbe41cba1a00d16b3aef6281a6817c2165
Wavator <40030837+Wavator@users.noreply.github.com>
2018-08-29 21:24:53 +0800

    Update Mo's.cpp

--- data_struct/Mo's.cpp
+++ data_struct/Mo's.cpp
@@ -1,49 +1,63 @@
-namespace mo
+class Mo
 {
+public:
     struct query {
-        int l, r, id;
-    };
-    const int N = 50010, M = 50010;
-    query q[M];
-    int pos[N];
-    int a[N];
-    int ans[N],now;
-    
-    inline void add(int x) {
-
-    }
-
-    inline void del(int x) {
-
-    }
-
-    int n, Q;
-
-    void main() {
-        int blk = int(sqrt(n));
-        while (blk * blk < n) blk++;
-        for (int i = 1; i <= n; ++i) {
-            pos[i] = (i - 1) / blk + 1;
+        int l, r, id, lb;
+        bool operator<(const query &o) const {
+            return lb==o.lb?(((lb)&1)?r<o.r:r>o.r): lb<o.lb;
         }
-        sort(q+1,q+Q+1,[&](const query &a, const query&b){
-            return pos[a.l]==pos[b.l]?a.r<b.r:pos[a.l]<pos[b.l];
-        });
+    }q[M];
+    int a[N];
+    int ans[N];
+    int n, Q, blk;
+    inline void add(int x)
+    {
+        x=a[x];
+    }
+    inline void del(int x)
+    {
+        x = a[x];
+    }
+    void init()
+    {
+        blk = (int)sqrt(n);
+        // read a, base 1
+        // read Q, base 1
+        for(int i=1;i<=Q;++i){
+            sc(q[i].l,q[i].r);
+            q[i].id=i;
+            q[i].lb=q[i].l/blk;
+        }
+        sort(q + 1, q + Q + 1);
+
+    }
+    int now;
+    void solve()
+    {
+        sort(q + 1, q + Q + 1);
         int l = 1, r = 0;
-        for (int i = 1; i <= Q; ++i) {
+        for (int i = 1; i <= Q; ++i)
+        {
             int L = q[i].l, R = q[i].r;
-            if(r < R){
+            if(r < R)
+            {
                 for(r++;r<=R;add(r++));
                 --r;
             }
             if(r>R)
+            {
                 for(; r>R; del(r--));
-            if(l<L)for(;l < L;del(l++));
-            if(l>L){
+            }
+            if(l<L)
+            {
+                for(;l < L;del(l++));
+            }
+            if(l>L)
+            {
                 for(l--; l >= L;add(l--));
                 l++;
             }
             ans[q[i].id]=now;
         }
-        for(int i=1;i<=Q;++i)printf("%d\n",ans[i]);
     }
-}
+};
