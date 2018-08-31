4656dd0ce61c32ff8f866331854f5bb665f34f3b
Wavator <40030837+Wavator@users.noreply.github.com>
2018-09-01 04:43:20 +0800

    Update BM.cpp

--- math/BM.cpp
+++ math/BM.cpp
@@ -1,62 +1,127 @@
 namespace linear_seq { //xudyh's BM
-    const int N=10010;
-    ll res[N],base[N],_c[N],_md[N];
-    vector<int> Md;
-    void mul(ll *a,ll *b,int k) {
-        rep(i,0,k+k) _c[i]=0;
-        rep(i,0,k) if (a[i]) rep(j,0,k) _c[i+j]=(_c[i+j]+a[i]*b[j])%mod;
-        for (int i=k+k-1;i>=k;i--) if (_c[i])
-                rep(j,0,SZ(Md)) _c[i-k+Md[j]]=(_c[i-k+Md[j]]-_c[i]*_md[Md[j]])%mod;
-        rep(i,0,k) a[i]=_c[i];
+    const int N = 10010;
+    typedef long long ll;
+    constexpr ll mod = (ll) 1e9 + 7;
+
+    ll pow_mod(ll a, ll b) {
+        ll r = 1;
+        for (a %= mod; b; b >>= 1, a = a * a % mod) {
+            if (b & 1)r = r * a % mod;
+        }
+        return r;
     }
-    int solve(ll n,vi a,vi b) { // a 系数 b 初值 b[n+1]=a[0]*b[n]+...
-//        printf("SIZE %d\n",SZ(b));
-        ll ans=0,pnt=0;
-        int k=SZ(a);
-        assert(SZ(a)==SZ(b));
-        rep(i,0,k) _md[k-1-i]=-a[i];_md[k]=1;
-        Md.clear();
-        rep(i,0,k) if (_md[i]!=0) Md.push_back(i);
-        rep(i,0,k) res[i]=base[i]=0;
-        res[0]=1;
-        while ((1ll<<pnt)<=n) pnt++;
-        for (int p=pnt;p>=0;p--) {
-            mul(res,res,k);
-            if ((n>>p)&1) {
-                for (int i=k-1;i>=0;i--) res[i+1]=res[i];res[0]=0;
-                rep(j,0,SZ(Md)) res[Md[j]]=(res[Md[j]]-res[k]*_md[Md[j]])%mod;
+
+    ll res[N], base[N], _c[N], _md[N];
+    vector<int> Md;
+
+    void mul(ll *a, ll *b, int k) {
+        k <<= 1;
+        for (int i = 0; i < k; ++i) _c[i] = 0;
+        k >>= 1;
+        for (int i = 0; i < k; ++i) {
+            if (a[i]) {
+                for (int j = 0; j < k; ++j) {
+                    _c[i + j] = (_c[i + j] + a[i] * b[j]) % mod;
+                }
             }
         }
-        rep(i,0,k) ans=(ans+res[i]*b[i])%mod;
-        if (ans<0) ans+=mod;
+        for (int i = k + k - 1; i >= k; i--) {
+            if (_c[i]) {
+                for (const int md: Md) {
+                    _c[i - k + md] = (_c[i - k + md] - _c[i] * _md[md]) % mod;
+                }
+            }
+        }
+        for (int i = 0; i < k; ++i) {
+            a[i] = _c[i];
+        }
+    }
+
+    int solve(ll n, vector<int> a, vector<int> b) { // a 系数 b 初值 b[n+1]=a[0]*b[n]+...
+//        printf("SIZE %d\n",SZ(b));
+        ll ans = 0, pnt = 0;
+        int k = (int) a.size();
+        assert(a.size() == b.size());
+        for (int i = 0; i < k; ++i) {
+            _md[k - 1 - i] = -a[i];
+        }
+        _md[k] = 1;
+        Md.clear();
+        for (int i = 0; i < k; ++i) {
+            if (_md[i] != 0) {
+                Md.push_back(i);
+            }
+        }
+        for (int i = 0; i < k; ++i) {
+            res[i] = base[i] = 0;
+        }
+        res[0] = 1;
+        while ((1ll << pnt) <= n) {
+            pnt++;
+        }
+        for (int p = pnt; p >= 0; p--) {
+            mul(res, res, k);
+            if ((n >> p) & 1) {
+                for (int i = k - 1; i >= 0; i--) {
+                    res[i + 1] = res[i];
+                }
+                res[0] = 0;
+                for (const int md: Md) {
+                    res[md] = (res[md] - res[k] * _md[md]) % mod;
+                }
+            }
+        }
+        for (int i = 0; i < k; ++i) {
+            ans = (ans + res[i] * b[i]) % mod;
+        }
+        if (ans < 0) ans += mod;
         return ans;
     }
-    vi BM(vi s) {
-        vi C(1,1),B(1,1);
-        int L=0,m=1,b=1;
-        rep(n,0,SZ(s)) {
-            ll d=0;
-            rep(i,0,L+1) d=(d+(ll)C[i]*s[n-i])%mod;
-            if (d==0) ++m;
-            else if (2*L<=n) {
-                vi T=C;
-                ll c=mod-d*pow_mod(b,mod-2)%mod;
-                while (SZ(C)<SZ(B)+m) C.pb(0);
-                rep(i,0,SZ(B)) C[i+m]=(C[i+m]+c*B[i])%mod;
-                L=n+1-L; B=T; b=d; m=1;
+
+    vector<int> BM(vector<int> s) {
+        vector<int> C(1, 1), B(1, 1);
+        int L = 0, m = 1, b = 1;
+        for (int n = 0; n < (int) s.size(); ++n) {
+            ll d = 0;
+            for (int i = 0; i <= L; ++i) {
+                d = (d + (ll) C[i] * s[n - i]) % mod;
+            }
+            if (d == 0) {
+                ++m;
+            }
+            else if (2 * L <= n) {
+                vector<int> T = C;
+                ll c = mod - d * pow_mod(b, mod - 2) % mod;
+                while (C.size() < B.size() + m) {
+                    C.push_back(0);
+                }
+                for (int i = 0; i < (int) B.size(); ++i) {
+                    C[i + m] = (C[i + m] + c * B[i]) % mod;
+                }
+                L = n + 1 - L;
+                B = T;
+                b = d;
+                m = 1;
             } else {
-                ll c=mod-d*pow_mod(b,mod-2)%mod;
-                while (SZ(C)<SZ(B)+m) C.pb(0);
-                rep(i,0,SZ(B)) C[i+m]=(C[i+m]+c*B[i])%mod;
+                ll c = mod - d * pow_mod(b, mod - 2) % mod;
+                while (C.size() < B.size() + m) {
+                    C.push_back(0);
+                }
+                for (int i = 0; i < (int) B.size(); ++i) {
+                    C[i + m] = (C[i + m] + c * B[i]) % mod;
+                }
                 ++m;
             }
         }
         return C;
     }
-    int gao(vi a,ll n) {
-        vi c=BM(a);
+
+    int gao(vector<int> a, ll n) {
+        vector<int> c = BM(a);
         c.erase(c.begin());
-        rep(i,0,SZ(c)) c[i]=(mod-c[i])%mod;
-        return solve(n,c,vi(a.begin(),a.begin()+SZ(c)));
+        for (int &x:c) {
+            x = (mod - x) % mod;
+        }
+        return solve(n, c, vector<int>(a.begin(), a.begin() + c.size()));
     }
 }
