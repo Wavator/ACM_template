f84d220d828ca48629ad1e36ebef338cd9e8f5e6
Wavator <40030837+Wavator@users.noreply.github.com>
2018-09-01 04:48:14 +0800

    Update normal.cpp

--- normal.cpp
+++ normal.cpp
@@ -19,7 +19,21 @@ using namespace std;
 #define fi first
 #define se second
 typedef long long ll;
+typedef unsigned long long ull;
 typedef double db;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+constexpr db PI = acos(-1), eps = 1e-8;
+constexpr ll mod = (ll)1e9 + 7;
+ll pow_mod(ll a, ll b) {
+    assert(b >= 0);
+    ll r = 1;
+    for (a %= mod; b; b >>= 1, a = a * a % mod) {
+        if (b & 1) {
+            r = r * a % mod;
+        }
+    }
+    return r;
+}
