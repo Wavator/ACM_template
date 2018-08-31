a8a2f48a3342ca7c548d13fb7ef71ffd07b80194
Wavator <40030837+Wavator@users.noreply.github.com>
2018-09-01 04:32:29 +0800

    Update poly_sum.cpp

--- math/poly_sum.cpp
+++ math/poly_sum.cpp
@@ -1,51 +1,88 @@
-#define rep(i, a, b) for (int i = (a); i < (b); ++i)
-#define per(i, a, b) for (int i = (b - 1); i >= a; --i)
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-#define SZ(x) (int)(x).size()
-typedef long long ll;
-typedef double db;
-typedef vector<int> vi;
+/*
+ * 杜教插值，一看就很牛逼
+ */
+namespace poly_sum {
+    const int D = 101000;
+    typedef long long ll;
+    constexpr ll mod = (ll) 1e9 + 7;
 
-const ll mod = (ll)1e9 + 7;
-ll pow_mod(ll a, ll b) {
-    assert(b >= 0);
-    ll res = 1;
-    for (a %= mod; b; b >>= 1, a = a * a % mod) {
-        if (b & 1) {
-            res = res * a % mod;
+    ll pow_mod(ll a, ll b) {
+        ll r = 1;
+        for (; b; b >>= 1, a = a * a % mod) {
+            if (b & 1) {
+                r = r * a % mod;
+            }
         }
+        return r;
     }
-    return res;
-}
-namespace poly_sum { //rep/per is 0 based
-    const int D = 3010;
-    ll f[D], g[D], p1[D], p2[D];
-    ll calcn(int d, ll* a, ll n) { // d is the term number you have, n is the number of terms you want to calculate. return the ploy's sum.
-        if (n <= d)
-            return a[n];
+
+    ll fact[D], finv[D], p1[D], p2[D], h[D][2], C[D];
+
+    ll calcn(int d, ll *a, ll n) {
+        if (n <= d) return a[n];
         p1[0] = p2[0] = 1;
-        rep(i, 0, d + 1) {
+        for (int i = 0; i <= d; ++i) {
             ll t = (n - i + mod) % mod;
             p1[i + 1] = p1[i] * t % mod;
         }
-        rep(i, 0, d + 1) {
+        for (int i = 0; i <= d; ++i) {
             ll t = (n - d + i + mod) % mod;
             p2[i + 1] = p2[i] * t % mod;
         }
         ll ans = 0;
-        rep(i, 0, d + 1) {
-            ll t = g[i] * g[d-i] % mod * p1[i] % mod * p2[d - i] % mod * a[i] % mod;
-            if ((d - i) & 1) ans = (ans - t + mod) % mod;
-            else ans = (ans + t) % mod;
+        for (int i = 0; i <= d; ++i) {
+            ll t = finv[i] * finv[d - i] % mod * p1[i] % mod * p2[d - i] % mod * a[i] % mod;
+            if ((d - i) & 1) {
+                ans = (ans - t + mod) % mod;
+            } else ans = (ans + t) % mod;
         }
         return ans;
     }
+
     void init(int M) {
-        f[0] = f[1] = g[0] = g[1]=1;
-        rep(i, 2, M + 5) f[i] = f[i - 1] * i % mod;
-        g[M + 4] = pow_mod(f[M + 4], mod - 2);
-        per(i, 1, M + 4) g[i] = g[i + 1] * (i + 1) % mod;
+        fact[0] = fact[1] = finv[0] = finv[1] = 1;
+        for (int i = 2; i < M + 5; ++i) {
+            fact[i] = fact[i - 1] * i % mod;
+        }
+        finv[M + 4] = pow_mod(fact[M + 4], mod - 2);
+        for (int i = M + 3; i; --i) finv[i] = finv[i + 1] * (i + 1) % mod;
+    }
+
+    ll polysum(ll n, ll *a, ll m) { // a[0].. a[m] \sum_{i=0}^{n-1} a[i]
+        a[m + 1] = calcn(m, a, m + 1);
+        for (int i = 1; i < m + 2; ++i) {
+            a[i] = (a[i - 1] + a[i]) % mod;
+        }
+        return calcn(m + 1, a, n - 1);
+    }
+
+    ll qpolysum(ll R, ll n, ll *a, ll m) { // a[0].. a[m] \sum_{i=0}^{n-1} a[i]*R^i
+        if (R == 1) return polysum(n, a, m);
+        a[m + 1] = calcn(m, a, m + 1);
+        ll r = pow_mod(R, mod - 2), p3 = 0, p4 = 0, c, ans;
+        h[0][0] = 0;
+        h[0][1] = 1;
+        for (int i = 1; i < m + 2; ++i) {
+            h[i][0] = (h[i - 1][0] + a[i - 1]) * r % mod;
+            h[i][1] = h[i - 1][1] * r % mod;
+        }
+        for (int i = 0; i < m + 2; ++i) {
+            ll t = finv[i] * finv[m + 1 - i] % mod;
+            if (i & 1) {
+                p3 = ((p3 - h[i][0] * t) % mod + mod) % mod, p4 = ((p4 - h[i][1] * t) % mod + mod) % mod;
+            } else {
+                p3 = (p3 + h[i][0] * t) % mod, p4 = (p4 + h[i][1] * t) % mod;
+            }
+        }
+        c = pow_mod(p4, mod - 2) * (mod - p3) % mod;
+        for (int i = 0; i < m + 2; ++i) {
+            h[i][0] = (h[i][0] + h[i][1] * c) % mod;
+        }
+        for (int i = 0; i < m + 2; ++i) {
+            C[i] = h[i][0];
+        }
+        ans = (calcn(m, C, n) * pow_mod(R, n) - c) % mod;
+        if (ans < 0) ans += mod;
+        return ans;
     }
 }
