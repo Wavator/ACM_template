b0c5399f989fb376f0a767101840d38dfe6c3a21
Wavator <40030837+Wavator@users.noreply.github.com>
2018-09-05 18:23:39 +0800

    Update normal.cpp

--- normal.cpp
+++ normal.cpp
@@ -19,6 +19,7 @@ using namespace std;
 #define rep(i,a,n) for(int i=(a);i<(n);++i)
 #define per(i,a,n) for(int i=(n)-1;i>=(a);--i)
 #define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 #define SZ(x) ((int)(x).size())
 #define fi first
 #define se second
@@ -26,18 +27,23 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef double db;
 typedef vector<int> vi;
-typedef vector<vi> vvi;
+typedef vector<ll> vl;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 constexpr db PI = acos(-1), eps = 1e-8;
 constexpr ll mod = (ll)1e9 + 7;
 ll pow_mod(ll a, ll b) {
     assert(b >= 0);
-    ll r = 1;
+    ll res = 1;
     for (a %= mod; b; b >>= 1, a = a * a % mod) {
         if (b & 1) {
-            r = r * a % mod;
+            res = res * a % mod;
         }
     }
-    return r;
+    return res;
+}
+
+int main() {
+
+    return 0;
 }
