1e4fe6caa8d1ecaf8075d47845adae188724889c
Yu Zhao <40030837+Wavator@users.noreply.github.com>
2018-09-24 14:14:33 +0800

    Update dsu.cpp

--- data_struct/dsu.cpp
+++ data_struct/dsu.cpp
@@ -1,32 +1,37 @@
+template <size_t N>
 class DSU {
 public:
-    vector<int> p, r;
+    int p[N], r[N];
     void init(int n) {
-        p.resize(n+1);
-        r.resize(n+1);
         for(int i = 0; i <= n; i++) {
             p[i] = i;
             r[i] = 0;
         }
     }
+    
     int find(int x) {
         return p[x] == x? x: p[x] = find(p[x]);
     }
+    
     bool unite(int x,int y) {
         x = find(x);
         y = find(y);
-        if(x == y)
+        if(x == y) {
             return false;
-        if(r[x] < r[y])
+        }
+        if(r[x] < r[y]) {
             p[x] = y;
+        }
         else {
             p[y] = x;
-            if(r[x]==r[y])
+            if(r[x]==r[y]) {
                 r[x]++;
+            }
         }
         return true;
     }
+    
     bool same(int x,int y) {
-        return find(x)==find(y);
+        return find(x) == find(y);
     }
 };
